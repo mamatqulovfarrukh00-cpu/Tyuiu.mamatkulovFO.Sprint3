231a13e5394f6153e0facd8a3a201c538af1924c
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-18 13:05:54 +0500

    Task0

--- Tyuiu.mamatkulovFO.Sprint3.Task0.V21.Lib/DataService.cs
+++ Tyuiu.mamatkulovFO.Sprint3.Task0.V21.Lib/DataService.cs
@@ -5,14 +5,8 @@ namespace Tyuiu.mamatkulovF0.Sprint3.Task0.V21.Lib
 {
     public class DataService : ISprint3Task0V21
     {
-        public static double CalculateSum(double a)
-        {
-            throw new NotImplementedException();
-        }
-
         public double GetSumSeries(double value, int startValue, int stopValue)
         {
-            
             double sum = 0;
             for (int k = startValue; k <= stopValue; k++)
             {
@@ -21,8 +15,10 @@ namespace Tyuiu.mamatkulovF0.Sprint3.Task0.V21.Lib
             return sum;
         }
 
-        
+        public static double CalculateSum(double a)
+        {
+            return new DataService().GetSumSeries(a, 1, 13);
+        }
     }
 }
-
 
