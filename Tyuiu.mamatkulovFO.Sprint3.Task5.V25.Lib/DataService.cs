a20442586a4376734292449ad7bcecefc3cf3031
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-20 13:25:17 +0500

    Task5

--- Tyuiu.mamatkulovFO.Sprint3.Task5.V25.Lib/DataService.cs
+++ Tyuiu.mamatkulovFO.Sprint3.Task5.V25.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.mamatkulovFO.Sprint3.Task5.V25.Lib
 {
     public class DataService : ISprint3Task5V25
