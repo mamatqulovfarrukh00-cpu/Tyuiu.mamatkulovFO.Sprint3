44b0f51ac8d2ba7c71fd400f726c2f950ab08847
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-25 10:43:23 +0500

    Task6

--- Tyuiu.mamatkulovFO.Sprint3.Task6.V16/Program.cs
+++ Tyuiu.mamatkulovFO.Sprint3.Task6.V16/Program.cs
@@ -6,20 +6,9 @@ namespace Tyuiu.mamatkulovFO.Sprint3.Task6.V16
     {
         static void Main()
         {
-            Console.WriteLine("=== Вариант 16 ===");
-            Console.WriteLine("Поиск количества делителей чисел в диапазоне [5, 16]\n");
-
             var service = new DataService();
             var counts = service.GetDivisorCounts(5, 16);
-
-            foreach (var kvp in counts)
-            {
-                Console.WriteLine($"Число {kvp.Key} имеет {kvp.Value} делителей.");
-            }
-
-            Console.WriteLine($"\nОбщее количество делителей всех чисел: {counts.Values.Sum()}");
-            Console.WriteLine("Нажмите любую клавишу для выхода...");
-            Console.ReadKey();
+            Console.WriteLine(counts.Values.Sum()); // Chiqadi: 42
         }
     }
 }
